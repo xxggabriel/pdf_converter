52b8f543cf1c4ff9059b4aedcb15b977dc76ca56
Gabriel Oliveira de Souza <gabriel@umobi.com.br>
2025-02-06 19:00:57 -0300

    Remove arquivos agora ignorados pelo .gitignore

--- src/core/contracts/ifile_handler.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC, abstractmethod
-
-
-class IFileHandler(ABC):
-    
-    @abstractmethod
-    def save(content: str, output_path: str):
-        pass
--- src/handlers/file_handler.py
+++
@@ -1,9 +0,0 @@
-
-from core.contracts.ifile_handler import IFileHandler
-
-class FileHandler(IFileHandler):
-    @staticmethod
-    def save(content: str, output_path: str):
-        """Salva o conteúdo em arquivo"""
-        with open(output_path, "w", encoding="utf-8") as f:
-            f.write(content)
